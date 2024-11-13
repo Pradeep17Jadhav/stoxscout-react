80087b0ab3321c3919a998278a0b6896a4eddf0b
Pradeep Jadhav <pradeep17jadhav@gmail.com>
2024-11-13 23:30:08 +0530

    change font sizes to rem

--- src/components/HoldingInformation/HoldingInformation.tsx
+++ src/components/HoldingInformation/HoldingInformation.tsx
@@ -11,25 +11,25 @@ export const HoldingInformation = ({holdingSummary}: Props) => {
         <div className="holdingInfo-container">
             <div className="holdingInfo">
                 <Box className="holdingInfoItem">
-                    <div>Investment</div>
+                    <span>Investment</span>
                     <div className={parseInt(holdingSummary.totalInvestedValue) >= 0 ? 'profit' : 'loss'}>
                         {holdingSummary.totalInvestedValue}
                     </div>
                 </Box>
                 <Box className="holdingInfoItem" sx={{display: {xs: 'none', sm: 'none', md: 'flex'}}}>
-                    <div>Current</div>
+                    <span>Current</span>
                     <div className={parseInt(holdingSummary.totalCurrentValue) >= 0 ? 'profit' : 'loss'}>
                         {holdingSummary.totalCurrentValue}
                     </div>
                 </Box>
                 <Box className="holdingInfoItem">
-                    <div>P&L</div>
+                    <span>P&L</span>
                     <div className={parseInt(holdingSummary.totalPnl) >= 0 ? 'profit' : 'loss'}>
                         {holdingSummary.totalPnl} ({holdingSummary.totalPnlPercentage}%)
                     </div>
                 </Box>
                 <Box className="holdingInfoItem">
-                    <div>Day P&L</div>
+                    <span>Day P&L</span>
                     <div className={parseInt(holdingSummary.totalDayChange) >= 0 ? 'profit' : 'loss'}>
                         {holdingSummary.totalDayChange} ({holdingSummary.totalDayChangePercentage}
                         %)
